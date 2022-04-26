238e3f4b3e301fa5f6f4160ebd14b5a91f5a39ac
BrianCaetano <brian-caetano@hotmail.com>
2022-04-26 00:15:05 -0300

    feat: interfaces TS

---
+++ src/interfaces/IButton.ts
@@ -0,0 +1,5 @@
+export interface IButton {
+  onClick: Function,
+  page: number,
+  setPage: Function,
+}
---
+++ src/interfaces/IPokemon.ts
@@ -0,0 +1,6 @@
+export interface IPokemon {
+  name: string,
+  spriteFront: string,
+  type1: string,
+  type2: string,
+}

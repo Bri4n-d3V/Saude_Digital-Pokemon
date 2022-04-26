86dca098e89e88e4d848f96c47335e16f54dabb1
BrianCaetano <brian-caetano@hotmail.com>
2022-04-26 00:12:31 -0300

    feat: Adiciona rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,32 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Spinner } from "@chakra-ui/react";
+import Home from './pages/Home';
+import Details from './pages/Details';
+import Header from './components/Header';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Suspense fallback={
+        <Spinner
+          thickness='4px'
+          speed='0.65s'
+          emptyColor='gray.200'
+          color='blue.5000'
+          size='xl'
+        />
+        }>
+        <Header/>
+        <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/details/:name" element={<Details />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </ChakraProvider>
   )
 }
 

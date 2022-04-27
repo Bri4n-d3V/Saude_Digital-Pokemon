29f5a2c61cf72ee88366f1744263c3c7fd4f415e
BrianCaetano <brian-caetano@hotmail.com>
2022-04-27 15:40:09 -0300

    refactor: header com lib github icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
+import pokedexLogo from '../images/pokedexLogo.png'
+import { AiOutlineGithub } from 'react-icons/ai';
+import { Box, Link, Text } from '@chakra-ui/react';
+
 export default function Header() {
   return (
-    <div>
-      <header>Header</header>
-    </div>
+    <header>
+      <Link href='/'>
+      <img
+        src={pokedexLogo}
+        alt='pokedex logo'
+      />
+      </Link>
+      <Link display='flex' href='https://github.com/Bri4n-d3V' isExternal>
+        <Box display='flex' gap='0.5rem' alignItems='center'>
+          <Text fontSize='xl'>Bri4n-d3V</Text>
+          <AiOutlineGithub style={{fontSize: '30px'}}/>
+        </Box>
+      </Link>
+    </header>
   );
 }

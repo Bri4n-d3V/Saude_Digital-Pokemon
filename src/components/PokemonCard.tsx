a8f99a87ac9a19a1439ac1b07644a5f2e5261348
BrianCaetano <brian-caetano@hotmail.com>
2022-04-27 16:08:46 -0300

    refactor: style pokemoncard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,4 +1,4 @@
-import { Container } from '@chakra-ui/react';
+import { Container, Center } from '@chakra-ui/react';
 import { IPokemon } from '../interfaces/IPokemon';
 import { Link } from 'react-router-dom';
 
@@ -9,22 +9,31 @@ const PokemonCard: React.FC<IPokemon> = ({
   type2,
 }) => {
   return (
-    <Container>
-      <img src={spriteFront} />
-      <div>
-        <h4>{name[0].toUpperCase() + name.substr(1)}</h4>
-        <p>
-          {(type2 !== type1)
-            ? `Types: ${type1} e ${type2}`
-            : `Type: ${type1}`}
-        </p>
-        <Link
-          key={name}
-          to={`/details/${name}`}
-        >
-          <a href='https://www.google.com'>Details</a>
-        </Link>
-      </div>
+    <Container className='card-pokemon'>
+        <Center className='link-img'>
+          <Link
+              key={name}
+              to={`/details/${name}`}
+            >
+            <img src={spriteFront} />
+            </Link>
+        </Center>
+        <div>
+          <h4>{name[0].toUpperCase() + name.substr(1)}</h4>
+          <div className='type'>
+            {(type2 !== type1)
+              ? (<><p>{type1}</p> <p>{type2}</p></>)
+              : (<p>{type1}</p>)
+              }
+          </div>
+          <Link
+            key={name}
+            className='link'
+            to={`/details/${name}`}
+          >
+            <p>Details</p>
+          </Link>
+        </div>
     </Container>
   )
 }
